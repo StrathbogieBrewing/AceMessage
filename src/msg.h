0449392fd1f78140a7c062b317660859c34029f5
John <john@solarhot.org>
2021-05-26 08:36:38 +1000

    change to integer for message description

--- src/msg.h
+++ src/msg.h
@@ -6,15 +6,39 @@ extern "C" {
 #endif
 
 enum {
-  MSG_NULL = 0,
-  MSG_MILLI,     // make fixed point with 3 decimal places
-  MSG_CENTI,     // make fixed point with 2 decimal places
-  MSG_DECI,      // make fixed point with 1 decimal places
-  MSG_UNIT,      // display at least one digit
-  MSG_TWODIGITS, // display at least two digit
-  MSG_TIME,      // display as a time
-  MSG_BOOL,      // display as on / off
+  MSG_UNIT = 0x0000,      // display at least one digit
+  MSG_MILLI = 0x1000,     // make fixed point with 3 decimal places
+  MSG_CENTI = 0x2000,     // make fixed point with 2 decimal places
+  MSG_DECI = 0x3000,      // make fixed point with 1 decimal places
+  MSG_TWODIGITS = 0x4000, // display at least two digit
+  MSG_TIME = 0x5000,      // display as a time
+  MSG_BOOL = 0x6000,      // display as on / off
+  MSG_NULL = 0x7000,
 };
+
+#define MSG_FMT(M) (M & 0x8000)
+
+enum {
+  MSG_BYTE_0 = 0x0000,
+  MSG_BYTE_1 = 0x0100,
+  MSG_BYTE_2 = 0x0200,
+  MSG_BYTE_3 = 0x0300,
+  MSG_BYTE_4 = 0x0400,
+  MSG_BYTE_5 = 0x0500,
+  MSG_BYTE_6 = 0x0600,
+  MSG_BYTE_7 = 0x0700,
+};
+
+#define MSG_OFFSET(M) ((M & 0x0700) >> 8)
+
+enum {
+  MSG_BYTE = 0x0000,
+  MSG_WORD = 0x0800
+};
+
+#define MSG_SIZE(M) (M & 0x0800)
+
+#define MSG_ID(M) (M & 0x00FF)
 
 typedef struct {
   unsigned char msgID;
@@ -22,20 +46,13 @@ typedef struct {
 } msg_data_t;
 
 typedef struct {
-  const unsigned char msgID;
-  const unsigned char bitOffset;
-  const unsigned char bitCount;
-  const unsigned char format;
-} msg_pack_t;
-
-typedef struct {
   const char *name;
-  const msg_pack_t pack;
+  const int pack;
 } msg_name_t;
 
-void msg_pack(msg_data_t *data, msg_pack_t *pack, int value);
-int msg_unpack(msg_data_t *data, const msg_pack_t *pack, int *value);
-int msg_format(msg_data_t *data, const msg_pack_t *pack, char *str);
+void msg_pack(msg_data_t *data, int pack, int value);
+int msg_unpack(msg_data_t *data, const int pack, int *value);
+int msg_format(msg_data_t *data, const int pack, char *str);
 
 #ifdef __cplusplus
 } // extern "C"
